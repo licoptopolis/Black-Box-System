4a8d464b41af60980350acd671134167ca66a71c
Hassan <Hassan.hussain0q@gmail.com>
2023-07-20 19:30:11 +0100

    Update Moving_Average_TradingSystem.py
    Code will be updated soon

--- Moving_Average_TradingSystem.py
+++ Moving_Average_TradingSystem.py
@@ -5,6 +5,9 @@ of moving averages. 'If ma_1 crosses above ma_2 you get a buy signal,
 and vice versa.' Stock data is used from the yFinance API and,
 and the data is plotted using Matplotlib.
 """
+"""
+Trading Signals can be sent from and to an email address
+"""
 
 """Importing Libraries"""
 import datetime as dt
@@ -12,7 +15,6 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import pandas as pd
 plt.style.use("dark_background")  # Graph Theme
-
 pd.options.mode.chained_assignment = None  # Prevent Warnings from Being Displayed in Output
 
 """User Inputs"""
@@ -45,7 +47,7 @@ def inputs():
 ma_1, ma_2 = inputs()
 
 """Stock Time Line"""
-start = dt.datetime.now() - dt.timedelta(365 * 10)
+start = dt.datetime.now() - dt.timedelta(365 * 5)
 end = dt.datetime.now()
 
 """Data Input & Download"""
@@ -63,20 +65,57 @@ data = data.iloc[ma_2:]
 """Trading Signal System"""
 buy = []
 sell = []
+buy_signals = []
+sell_signals = []
 trigger = 0  # Identify Changes
 
 for x in range(len(data)):
+
     if data[f"SMA_{ma_1}"].iloc[x] > data[f"SMA_{ma_2}"].iloc[x] and trigger != 1:
         buy.append(data["Adj Close"].iloc[x])
         sell.append(float('nan'))
+        buy_signals.append(f"Buy Signal at {data.index[x]}: Price - {round(data['Adj Close'].iloc[x], 2)}")
         trigger = 1
     elif data[f"SMA_{ma_1}"].iloc[x] < data[f"SMA_{ma_2}"].iloc[x] and trigger != -1:
         buy.append(float('nan'))
         sell.append(data["Adj Close"].iloc[x])
+        sell_signals.append(f"Sell Signal at {data.index[x]}: Price - {round(data['Adj Close'].iloc[x],2)}")
         trigger = -1
     else:
         buy.append(float('nan'))
         sell.append((float('nan')))
+
+buy_noti = "\n".join(buy_signals)
+sell_noti = "\n".join(sell_signals)
+
+print(buy_noti)
+print(sell_noti)
+print("\033[1;31mClose Pyplot Chart to Send Signals to Email!.\033[m")
+
+"""SENDING EMAIL NOTIFICATIONS"""   # NOTE: Email will be sent once you close pyplot chart
+from email.message import EmailMessage
+import smtplib
+import ssl
+
+passwrd = '*ENTER PASSWORD'
+email_sender = 'ENTER SENDER EMAIL'
+email_pass = passwrd
+email_reciever = 'ENTER RECIEVERS EMAIL'
+subject = f"Trading Signals for {data_request}"
+buy_subheading = "Buy Signals:"
+sell_subheading = "Sell Signals:"
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_reciever
+em['Subject'] = subject
+em.set_content(f"{buy_subheading}\n{buy_noti}\n\n{sell_subheading}\n{sell_noti}")
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_pass)
+    smtp.sendmail(email_sender, email_reciever, em.as_string())
 
 """Plotting Data"""
 data["Buy Signals"] = buy
